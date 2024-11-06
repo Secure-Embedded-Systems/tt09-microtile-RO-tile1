6bb55eba1704c0dea916eaa550a64257ebd0c5a7
Roy1707018 <aroy1@wpi.edu>
2024-11-05 19:49:21 -0500

    update

--- src/tt_um_roy1707018_ro.v
+++ src/tt_um_roy1707018_ro.v
@@ -14,10 +14,10 @@ module tt_um_roy1707018_ro (
 `endif
     input  wire [7:0] ui_in,    // Dedicated inputs (we'll use ui_in[1:0] for mux control)
     output wire [7:0] uo_out,   // Dedicated outputs (8-bit output of time count)
-    input  wire [7:0] uio_in,   // IOs: Input path
-    output wire [7:0] uio_out,  // IOs: Output path
-    output wire [7:0] uio_oe,   // IOs: Enable path (active high: 0=input, 1=output)
-    input  wire       ena,      // Don't use (used for power gating)
+   // input  wire [7:0] uio_in,   // IOs: Input path
+   // output wire [7:0] uio_out,  // IOs: Output path
+    //output wire [7:0] uio_oe,   // IOs: Enable path (active high: 0=input, 1=output)
+    //input  wire       ena,      // Don't use (used for power gating)
     input  wire       clk,      // System clock
     input  wire       rst_n     // Active-low reset
 );
@@ -40,12 +40,12 @@ module tt_um_roy1707018_ro (
     // Output the selected 8 bits to uo_out
    // assign uo_out = selected_count;
     assign uo_out[7:0] = 8'b0;
-    assign uio_out = 0;
-    assign uio_oe = 0;
+    //assign uio_out = 0;
+    //assign uio_oe = 0;
 
 
   // List all unused inputs to prevent warnings
-  wire _unused = &{ena, uio_in, 1'b0};
+  //wire _unused = &{ena, uio_in, 1'b0};
 
 
 
